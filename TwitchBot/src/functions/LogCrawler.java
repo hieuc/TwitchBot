b16e6fe1f9a9af93cfab210b73861e79469dfdd5
hieuc <hieu2192012@gmail.com>
2020-04-28 00:42:36 -0700

    switch from html parsing to api

--- TwitchBot/src/functions/LogCrawler.java
+++ TwitchBot/src/functions/LogCrawler.java
@@ -24,10 +24,7 @@ import org.jsoup.select.Elements;
  */
 public class LogCrawler {
     /** Main source of log. */
-    private static final String HOME = "https://overrustlelogs.net/";
-    
-    /** Keyword to grab items from HTML */
-    private static final String KEY_CLASS_TAG = "list-group-item list-group-item-action";
+    private static final String HOME = "https://overrustlelogs.net";
     
     /**
      * MAIN TO CONTROL.
@@ -36,7 +33,7 @@ public class LogCrawler {
      */
     public static void main(String...strings) {
         String[] channels = {"Destiny", "Destinygg"};
-        downloadChannels(Arrays.asList(channels));
+        downloadChannels(channels);
         //downloadLogs();
     }
     
@@ -47,78 +44,56 @@ public class LogCrawler {
         long startTime = System.nanoTime();
         
         // grab list of channels
-        final Elements channels = grabList(HOME);
+        final Connection con = Jsoup.connect(HOME + "/api/v1/channels.json");
         
-        // iterate thru list of channels
-        for (Element c : channels) {
-            String channelUrl = c.attr("abs:href");
-            // grab list of months
-            downloadChannel(channelUrl);
-        }
-        
-        System.out.println("Time elapsed: " + (System.nanoTime() - startTime)/ 1_000_000_000 + "s");
-    }
-    
-    /**
-     * Download logs from a list of channels.
-     * 
-     * @param channels
-     */
-    private static void downloadChannels(List<String> channels) {
-        long startTime = System.nanoTime();
-        for (String c : channels) {
-            String channelUrl = String.format("https://overrustlelogs.net/%s chatlog", c);
-            
-            downloadChannel(channelUrl);
-        }
-        System.out.println("Time elapsed: " + (System.nanoTime() - startTime)/ 1_000_000_000 + "s");
-    }
-    
-    /**
-     * Download logs form a specific channel.
-     * 
-     * @param channel URL
-     */
-    private static void downloadChannel(String url) {
-        final Elements months = grabList(url);
-        
-        // iterate thru each months
-        for (Element m : months) {
-            String monthUrl = m.attr("abs:href");
-            // grab list of items
-            final Elements items = grabList(monthUrl);
-            
-            // iterate thru list of items
-            for (Element i : items) {
-                String itemUrl = i.attr("abs:href");
-                // start download text files
-                if (!itemUrl.contains("userlog")) {
-                    itemUrl += ".txt";
-                    download(itemUrl);
-                }
-            }
-        }
-    }
-    
-    /**
-     * Grab list of items.
-     * 
-     * @param url
-     * @return Elements
-     */
-    private static Elements grabList(String url) {
         try {
-            // establish connection and get document
-            final Document doc = Jsoup.connect(url).get();
-            // get list
-            return doc.getElementsByClass(KEY_CLASS_TAG);
-        } catch (Exception e) {
+            String res = con.ignoreContentType(true).execute().body();
+            String[] channels = res.replace("\"", "").replace("[", "").replace("]", "").split(",");
+            
+            downloadChannels(channels);
+            
+        } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        return null;
+        
+        System.out.println("Time elapsed: " + (System.nanoTime() - startTime)/ 1_000_000_000 + "s");
     }
+
     
+    /**
+     * Download logs form a list of channels.
+     * 
+     * @param channels
+     */
+    private static void downloadChannels(String[] channels) {
+        try {
+            for (String channel : channels) {
+                // grab the months
+                final Connection con = Jsoup.connect(String.format(HOME + "/api/v1/%s/months.json", channel));
+                String res = con.ignoreContentType(true).execute().body();
+                String[] months = res.replace("\"", "").replace("[", "").replace("]", "").split(",");
+                
+                // iterate thru each months
+                for (String month : months) {
+                    
+                    // grab list of dates/items
+                    final Connection con1 = Jsoup.connect(String.format(HOME + "/api/v1/%s/%s/days.json", channel, month));
+                    String res1 = con1.ignoreContentType(true).execute().body();
+                    String[] items = res1.replace("\"", "").replace("[", "").replace("]", "").split(",");
+                    
+                    // iterate thru list of items
+                    for (String item : items) {
+                        // start download text files
+                        download(String.format(HOME + "/%s chatlog/%s/%s", channel, month, item));
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     private static void download(String url) {
         System.out.println(url);
         try {
@@ -126,12 +101,14 @@ public class LogCrawler {
             String channel = parts[3];
             String month = parts[4];
             String textname = parts[5];
+            // prepare file
+            File file = new File(String.format(System.getProperty("user.dir") + "/chatlogs/%s/%s/%s", channel, month, textname));
+            file.getParentFile().mkdirs();
+            
             // connect to url
             URL website = new URL(url);
             ReadableByteChannel rbc = Channels.newChannel(website.openStream());
             // download text stream
-            File file = new File(String.format(System.getProperty("user.dir") + "/chatlogs/%s/%s/%s", channel, month, textname));
-            file.getParentFile().mkdirs();
             
             FileOutputStream fos = new FileOutputStream(file, false);
             fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
